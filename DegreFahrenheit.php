39e0fc0f89908772d3ff7bcc5b4ba9eed9253c4e
vfleischmann <victor.fleischmann99@gmail.com>
2020-09-14 09:19:33 +0200

    bonjour

--- DegreFahrenheit.php
+++ DegreFahrenheit.php
@@ -5,14 +5,21 @@
     <title>Fonction DF</title>
 </head>
 <body>
-<p>Voici la fonction pour convertir degré vers Fahrenheit <BR>
+<p>Voici la fonction pour convertir degré vers Fahrenheit : <BR>
 <?php
 function degresFahrenheit($C){
     $CF =(9/5)*$C+32;
     return $CF;
 }
 $degre = 36;
-echo "La conversion de $degre degrés en Fahrenheit donne : ".degresFahrenheit($degre)." F."
+echo "La conversion de $degre degrés en Fahrenheit donne : ".degresFahrenheit($degre)." F.<BR>";
+
+function Fahrenheitdegre($F){
+    $FC =($F-32)*5/9;
+    return $FC;
+}
+$degre1 = 95;
+echo "La conversion de $degre1 degrés Fahrenheit vers degrés donne : ".Fahrenheitdegre($degre1)." C."
 ?>
 </body>
 </html>
